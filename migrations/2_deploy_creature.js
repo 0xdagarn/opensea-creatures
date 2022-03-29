1ff038cf6d38bd04ecfc497e4892b1d10f7efd42
dagarn <dagarn@unopnd.com>
2022-03-29 10:08:59 +0900

    Make script simple

--- migrations/2_deploy_creature.js
+++ migrations/2_deploy_creature.js
@@ -2,16 +2,6 @@ require("dotenv").config();
 const Creature = artifacts.require("./Creature.sol");
 const CreatureFactory = artifacts.require("./CreatureFactory.sol");
 
-// If you want to hardcode what deploys, comment out process.env.X and use
-// true/false;
-const DEPLOY_ALL = process.env.DEPLOY_ALL;
-const DEPLOY_ACCESSORIES_SALE = process.env.DEPLOY_ACCESSORIES_SALE || DEPLOY_ALL;
-const DEPLOY_ACCESSORIES = process.env.DEPLOY_ACCESSORIES || DEPLOY_ACCESSORIES_SALE || DEPLOY_ALL;
-const DEPLOY_CREATURES_SALE = process.env.DEPLOY_CREATURES_SALE || DEPLOY_ALL;
-// Note that we will default to this unless DEPLOY_ACCESSORIES is set.
-// This is to keep the historical behavior of this migration.
-const DEPLOY_CREATURES = process.env.DEPLOY_CREATURES || DEPLOY_CREATURES_SALE || DEPLOY_ALL || (! DEPLOY_ACCESSORIES);
-
 module.exports = async (deployer, network) => {
   // OpenSea proxy registry addresses for rinkeby and mainnet.
   let proxyRegistryAddress = "";
@@ -21,15 +11,10 @@ module.exports = async (deployer, network) => {
     proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
   }
 
-  if (DEPLOY_CREATURES) {
-    await deployer.deploy(Creature, proxyRegistryAddress, {gas: 5000000});
-  }
-
-  if (DEPLOY_CREATURES_SALE) {
-    await deployer.deploy(CreatureFactory, proxyRegistryAddress, Creature.address, {gas: 7000000});
-    const creature = await Creature.deployed();
-    await creature.transferOwnership(CreatureFactory.address);
-  }
+  await deployer.deploy(Creature, proxyRegistryAddress, {gas: 5000000});
+  await deployer.deploy(CreatureFactory, proxyRegistryAddress, Creature.address, {gas: 7000000});
+  const creature = await Creature.deployed();
+  await creature.transferOwnership(CreatureFactory.address);
 
   console.log("Deploying is finished. 👏👏👏");
 };
