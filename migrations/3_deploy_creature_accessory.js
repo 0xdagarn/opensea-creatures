edc08a60e29d3d80a9a2e2232b6bd3c64b5531bd
dagarn <dagarn@unopnd.com>
2022-04-01 14:49:34 +0900

    Refactor script

--- migrations/3_deploy_creature_accessory.js
+++ migrations/3_deploy_creature_accessory.js
@@ -26,43 +26,38 @@ module.exports = async (deployer, network, addresses) => {
     proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
   }
 
-  if (DEPLOY_ACCESSORIES) {
-    await deployer.deploy(
-      CreatureAccessory,
-      proxyRegistryAddress,
-      { gas: 5000000 }
-    );
-    const accessories = await CreatureAccessory.deployed();
-    await setupCreatureAccessories.setupAccessory(
-      accessories,
-      addresses[0]
-    );
-  }
+  await deployer.deploy(
+    CreatureAccessory,
+    proxyRegistryAddress,
+    { gas: 5000000 }
+  );
+  const accessories = await CreatureAccessory.deployed();
+  await setupCreatureAccessories.setupAccessory(
+    accessories,
+    addresses[0]
+  );
 
-  if (DEPLOY_ACCESSORIES_SALE) {
-    await deployer.deploy(LootBoxRandomness);
-    await deployer.link(LootBoxRandomness, CreatureAccessoryLootBox);
-    await deployer.deploy(
-      CreatureAccessoryLootBox,
-      proxyRegistryAddress,
-      { gas: 6721975 }
-    );
-    const lootBox = await CreatureAccessoryLootBox.deployed();
-    await deployer.deploy(
-      CreatureAccessoryFactory,
-      proxyRegistryAddress,
-      CreatureAccessory.address,
-      CreatureAccessoryLootBox.address,
-      { gas: 5000000 }
-    );
-    const accessories = await CreatureAccessory.deployed();
-    const factory = await CreatureAccessoryFactory.deployed();
-    await accessories.transferOwnership(
-      CreatureAccessoryFactory.address
-    );
-    await setupCreatureAccessories.setupAccessoryLootBox(lootBox, factory);
-    await lootBox.transferOwnership(factory.address);
-  }
+  await deployer.deploy(LootBoxRandomness);
+  await deployer.link(LootBoxRandomness, CreatureAccessoryLootBox);
+  await deployer.deploy(
+    CreatureAccessoryLootBox,
+    proxyRegistryAddress,
+    { gas: 6721975 }
+  );
+  const lootBox = await CreatureAccessoryLootBox.deployed();
+  await deployer.deploy(
+    CreatureAccessoryFactory,
+    proxyRegistryAddress,
+    CreatureAccessory.address,
+    CreatureAccessoryLootBox.address,
+    { gas: 5000000 }
+  );
+  const factory = await CreatureAccessoryFactory.deployed();
+  await accessories.transferOwnership(
+    CreatureAccessoryFactory.address
+  );
+  await setupCreatureAccessories.setupAccessoryLootBox(lootBox, factory);
+  await lootBox.transferOwnership(factory.address);
 
   console.log("Deploying is finished. 👏👏👏");
 };
